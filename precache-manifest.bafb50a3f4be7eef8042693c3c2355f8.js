629d809dd85bfa6c36e3dcf88de1b2967fcb0afd
AntonVak <antonv.snab@gmail.com>
2023-03-25 13:42:15 +0200

    loading build

---
+++ precache-manifest.bafb50a3f4be7eef8042693c3c2355f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e51f8906fe06fa747afcb053114ebb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7109e65e637c2e3e6f8",
+    "url": "/static/css/main.564d01d5.chunk.css"
+  },
+  {
+    "revision": "890f9589173509d9d5e2",
+    "url": "/static/js/2.9051bd81.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.9051bd81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7109e65e637c2e3e6f8",
+    "url": "/static/js/main.9bcf4a0e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.9bcf4a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4054bc057732f69f878d",
+    "url": "/static/js/runtime-main.13ba5424.js"
+  }
+]);
